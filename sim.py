09047a4eb7b1ba42b3ae2f14828b2bc0426736ea
Daniel-Thomp <danielgthompson10@gmail.com>
2025-10-25 14:25:05 +0100

    renamed

--- sim.py
+++ sim.py
@@ -2,9 +2,10 @@ import random
 from itertools import product
 values = ["Ace",2,3,4,5,6,7,8,9,10,"Jack","Queen","King"]
 suits = ["Hearts","Diamonds","Spades","Clubs"]
-
 cards = list(product(values,suits))
 random.shuffle(cards)
+
+game_state
 
 def get_card(n):
     hand = []
@@ -16,3 +17,5 @@ def print_hand(hand):
     for value,suit in hand:
         print(f"{value} of {suit}")
         
+def turn():
+    
